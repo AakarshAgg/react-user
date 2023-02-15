81fc5234d67ee1ddf87fab510919917715218a89
biber <biber4849@gmail.com>
2023-02-15 18:44:22 +0530

    Updates

---
+++ precache-manifest.0fa228015d9baa059794e6874a88f55a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0c82f17a02cded716b13298fc2550780",
+    "url": "/%20react-user/index.html"
+  },
+  {
+    "revision": "9567bf45cc2683f6a046",
+    "url": "/%20react-user/static/css/2.89f067aa.chunk.css"
+  },
+  {
+    "revision": "e4fe96ca8aecc0ee961c",
+    "url": "/%20react-user/static/css/main.932c2beb.chunk.css"
+  },
+  {
+    "revision": "9567bf45cc2683f6a046",
+    "url": "/%20react-user/static/js/2.5e66a8bb.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/%20react-user/static/js/2.5e66a8bb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e4fe96ca8aecc0ee961c",
+    "url": "/%20react-user/static/js/main.e6998476.chunk.js"
+  },
+  {
+    "revision": "7d79986a95a645315e8c",
+    "url": "/%20react-user/static/js/runtime-main.41cf95c4.js"
+  }
+]);
